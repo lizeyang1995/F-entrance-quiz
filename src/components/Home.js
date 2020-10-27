5b30e4622212808e9e786a7ea58a92d3449b0655
lizeyang1995 <424011527@qq.com>
2020-10-27 19:33:36 +0800

    [lizeyang] feat: add home css

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,15 @@
 import React, { Component } from 'react';
+import '../style/home.scss';
 
 class Home extends Component {
   render() {
     return (
       <div className="home">
         <div className="group-list">
-          <h3>分组列表</h3>
-          <button type="button">分组学员</button>
+          <div className="header">
+            <h3>分组列表</h3>
+            <button type="button">分组学员</button>
+          </div>
         </div>
         <div className="student-list">
           <h3>学员列表</h3>
